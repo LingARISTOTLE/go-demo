85d81c4631bbb085a1abe868f55855f1fc7fae43
ling <yl2397328561@163.com>
2022-11-14 21:11:17 +0800

    完成简单登录，但是对于路由没有保护，对于token死数据

--- controller/UserController.go
+++ controller/UserController.go
@@ -6,6 +6,7 @@ import (
 	"ginDemo/util"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
 )
@@ -21,11 +22,13 @@ func Register(context *gin.Context) {
 	//数据认证
 	if len(telephone) != 11 {
 		context.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号不是11位"})
+		return
 	}
 
 	//数据认证
 	if len(password) < 6 {
 		context.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+		return
 	}
 
 	//判断手机号是否存在
@@ -46,19 +49,70 @@ func Register(context *gin.Context) {
 	log.Println(username, password, telephone)
 
 	//创建用户
+	//首先修改密码，我们需要对用户密码进行加密
+	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		context.JSON(500, gin.H{"code": 500, "msg": "加密错误"})
+		return
+	}
 	newUser := model.User{
 		Username:  username,
 		Telephone: telephone,
-		Password:  password,
+		Password:  string(hasedPassword),
 	}
 
 	//将用户注册进数据库
 	db.Create(&newUser)
 
 	context.JSON(http.StatusOK, gin.H{
-		"msg": "注册成功",
+		"code": 200,
+		"msg":  "注册成功",
 	})
 
+}
+
+func Login(context *gin.Context) {
+	db := common.GetDB()
+	//获取参数
+	telephone := context.PostForm("telephone")
+	password := context.PostForm("password")
+
+	//数据认证
+	if len(telephone) != 11 {
+		context.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号不是11位"})
+		return
+	}
+
+	//数据认证
+	if len(password) < 6 {
+		context.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+		return
+	}
+
+	//判断手机号是否存在
+	var user model.User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.Id == 0 {
+		context.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户不存在"})
+		return
+	}
+
+	//此时数据库中的密码是加密后的密码
+	//判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"code": 400, "msg": "密码错误"})
+		return
+	}
+
+	//发放token
+	token := "11" //目前先写死数据
+
+	//返回结果
+	context.JSON(200, gin.H{
+		"code": 200,
+		"data": gin.H{"token": token},
+		"msg":  "登录成功",
+	})
 }
 
 func isTelephoneExist(db *gorm.DB, telephone string) bool { //查询手机号
--- routes.go
+++ routes.go
@@ -6,7 +6,11 @@ import (
 )
 
 func CollectRoute(r *gin.Engine) *gin.Engine { //该方法用于绑定路由
+	//注册事件
 	r.POST("/auto/register", controller.Register)
+
+	//登录事件
+	r.POST("/auto/login", controller.Login)
 
 	return r
 }
