8224a801d1a4d49c69deb69e9063e96875de749f
ling <yl2397328561@163.com>
2022-11-21 20:19:55 +0800

    完成配置文件动态加载，使用go get github.com/spf13/viper来进行配置文件读取操作

--- common/database.go
+++ common/database.go
@@ -3,18 +3,20 @@ package common
 import (
 	"fmt"
 	"github.com/jinzhu/gorm"
+	"github.com/spf13/viper"
 )
 
 var DB *gorm.DB
 
 func InitDB() *gorm.DB { //初始化数据库连接词
-	driverName := "mysql"
-	host := "localhost"
-	port := "3306"
-	database := "gintest"
-	username := "root"
-	password := "239732"
-	charset := "utf8"
+	//从配置文件获取gorm配置信息
+	driverName := viper.GetString("datasource.driverName")
+	host := viper.GetString("datasource.host")
+	port := viper.GetString("datasource.port")
+	database := viper.GetString("datasource.database")
+	username := viper.GetString("datasource.username")
+	password := viper.GetString("datasource.password")
+	charset := viper.GetString("datasource.charset")
 	//连接args和java的类似，一条成形的如下:
 	//"root:root123@tcp(127.0.0.1:3306)/test_gorm?charset=utf8mb4&parseTime=True&loc=Local"
 	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"ginDemo/common"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
+	"os"
 )
 
 //    import _ "github.com/jinzhu/gorm/dialects/mysql"
@@ -11,7 +13,15 @@ import (
 //    // import _ "github.com/jinzhu/gorm/dialects/sqlite"
 //    // import _ "github.com/jinzhu/gorm/dialects/mssql"
 
+//import (
+//	_ "github.com/jinzhu/gorm/dialects/mysql"
+//	"github.com/spf13/viper"
+//	"jkdev.cn/api/common"
+//)
+
 func main() {
+	//项目启动立马读取配置文件
+	InitConfig()
 	//获取数据库连接
 	db := common.InitDB()
 	//程序执行完出栈执行关闭
@@ -21,6 +31,28 @@ func main() {
 	ginServer := gin.Default()
 	//绑定连接路由
 	ginServer = CollectRoute(ginServer)
+
+	port := viper.GetString("server.port")
+	if port != "" {
+		port = ":" + port
+	} else {
+		port = ":8080"
+	}
 	//抓取异常
-	panic(ginServer.Run(":8083"))
+	panic(ginServer.Run(port))
+}
+
+func InitConfig() { //初始化配置文件
+	workDir, _ := os.Getwd()
+	//设置配置文件名称
+	viper.SetConfigName("application")
+	//设置文件类型
+	viper.SetConfigType("yml")
+	//设置文件目录
+	viper.AddConfigPath(workDir + "/config")
+	//读取配置文件
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic("")
+	}
 }
